f9555a72d5b5cca0b3304d3a24645cec372ecaf0
zf <17765114126@163.com>
2024-10-31 10:19:38 +0800

    db

--- web_api/run_api.py
+++ web_api/run_api.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
-import uvicorn
+from pydantic import BaseModel, Field
 from db import SQLiteDB
+import uvicorn
 import os
-from Req import BaseReq
 from Do import Contents
 
 app = FastAPI()
 
 we_library = SQLiteDB.SQLiteDB(os.path.join('..', 'db', 'we_library.db'))
+
+
+class BaseReq(BaseModel):
+    table_name: str = "contents"
+    page: int = Field(default=1, ge=1)  # 默认值为1，且必须大于等于1
+    page_size: int = Field(default=10, ge=-1)  # 默认值为10，允许-1表示不分页
+
+    class Config:
+        extra = 'allow'  # 允许额外的字段
 
 
 # 分页查询
